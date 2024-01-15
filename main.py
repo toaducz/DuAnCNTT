250971789f1d7c350bd1fb3ffb403a7f62f0b92c
toaducz <114118853+toaducz@users.noreply.github.com>
2024-01-15 13:48:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,10 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 tokenizer1 = AutoTokenizer.from_pretrained("toanduc/vit5-base-vietnews-summarization-finetuned")
 model1 = AutoModelForSeq2SeqLM.from_pretrained("toanduc/vit5-base-vietnews-summarization-finetuned")
 
-tokenizer2 = AutoTokenizer.from_pretrained("toanduc/bartpho-syllable-finetuned-sport-vietnamese")
-model2 = AutoModelForSeq2SeqLM.from_pretrained("toanduc/bartpho-syllable-finetuned-sport-vietnamese")
-
-tokenizer3 = AutoTokenizer.from_pretrained("toanduc/mT5_multilingual_XLSum-finetuned-sport-vietnamese-1")
-model3 = AutoModelForSeq2SeqLM.from_pretrained("toanduc/mT5_multilingual_XLSum-finetuned-sport-vietnamese-1")
-
 def main():
     
-    st.title("Tóm Tắt Văn Bản L903-1")
+    
+    st.title("Tóm Tắt Văn Bản Webapp")
     
     # Chọn phương thức nhập liệu từ người dùng
     input_method = st.radio("Chọn Phương Thức Nhập Liệu:", ("Tải Lên File", "Nhập Tay"))
@@ -37,28 +32,15 @@ def main():
         # Widget để nhập văn bản
         text_input = st.text_area("Nhập văn bản cần tóm tắt:", "")
 
-    if st.button("Tóm Tắt bằng ViT5"):
+    if st.button("Tóm Tắt"):
         # Gọi hàm tóm tắt văn bản ở đây (thay bằng code thực hiện tóm tắt)
-        summarized_text = perform_summarization_t5(text_input)
+        summarized_text = perform_summarization(text_input)
 
-        st.subheader("Kết Quả Tóm Tắt:")
-        st.write(summarized_text)
-    
-    if st.button("Tóm Tắt bằng Bart"):
-        # Gọi hàm tóm tắt văn bản ở đây (thay bằng code thực hiện tóm tắt)
-        summarized_text = perform_summarization_Bart(text_input)
-
-        st.subheader("Kết Quả Tóm Tắt:")
-        st.write(summarized_text)
-        
-    if st.button("Tóm Tắt bằng mT5"):
-        # Gọi hàm tóm tắt văn bản ở đây (thay bằng code thực hiện tóm tắt)
-        summarized_text = perform_summarization_mT5(text_input)
-
+        # Hiển thị kết quả tóm tắt
         st.subheader("Kết Quả Tóm Tắt:")
         st.write(summarized_text)
 
-def perform_summarization_t5(sentence):
+def perform_summarization(sentence):
     inputs = tokenizer1.encode("summarize: " + sentence, return_tensors="pt", max_length=512, truncation=True)
 
     summary_ids1 = model1.generate(inputs, max_length=200, length_penalty=2.0, num_beams=4, early_stopping=True, no_repeat_ngram_size=3)
@@ -68,22 +50,5 @@ def perform_summarization_t5(sentence):
 
     return summarized_text
 
-def perform_summarization_Bart(sentence):
-    inputs = tokenizer2.encode("summarize: " + sentence, return_tensors="pt", max_length=512, truncation=True)
-
-    summary_ids2 = model2.generate(inputs, max_length=200, length_penalty=2.0, num_beams=4, early_stopping=True)
-    summary_text_bart = tokenizer2.decode(summary_ids2[0], skip_special_tokens=True)
-    
-    summarized_text = summary_text_bart
-    
-def perform_summarization_mT5(sentence):
-    inputs = tokenizer3.encode("summarize: " + sentence, return_tensors="pt", max_length=512, truncation=True)
-
-    summary_ids4 = model3.generate(inputs, max_length=150, length_penalty=2.0, num_beams=4, early_stopping=True)
-    summary_text_mt5 = tokenizer3.decode(summary_ids4[0], skip_special_tokens=True)
-    
-    summarized_text = summary_text_mt5
-
-    return summarized_text
 if __name__ == "__main__":
     main()
